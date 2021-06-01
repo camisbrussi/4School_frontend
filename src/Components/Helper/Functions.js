d6084524584cf056e71d9b65e351930442259855
Chris <christiankroth@gmail.com>
2021-06-01 19:25:17 -0300

    Implementação da tabela no feed de atividades dos professores/responsáveis.

--- src/Components/Helper/Functions.js
+++ src/Components/Helper/Functions.js
@@ -3,6 +3,17 @@ export const formata_data_hora = (datetime) => {
     return date.toLocaleString("pt-BR");
 }
 
+export const formata_data_hora_para_datetime = (datahora) => {
+    let data = datahora.split(" ")[0];
+    let hora = datahora.split(" ")[1];
+
+    let dia = data.split("/")[0];
+    let mes = data.split("/")[1];
+    let ano = data.split("/")[2];
+
+    return ano + '-' + ("0"+mes).slice(-2) + '-' + ("0"+dia).slice(-2) + " " + hora;
+}
+
 export const formata_cpf = (cpf) => {
     cpf = cpf.replace(/[^\d]/g, "");
     return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
